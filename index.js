ddb3788eb8c4635da63b548541c83a89e6b73db3
rayana <dev38.osasbh@outlook.com>
2021-11-04 16:41:15 -0400

    Refactor code

--- index.js
+++ index.js
@@ -3,51 +3,51 @@ const Products = data.products
 const Categories = data.categories
 const Establishments = data.establishments
 var resultJson = {}
-console.log(handleJson(data))
-
-function handleJson(json){
-    Establishments.forEach(establishments => {
-        var categoriesNamesObjects = {}
-        const establishmentProductIdList = establishments.productsId
-        const filteredProducts = getProducts(establishmentProductIdList)
+function handleJson(){    
+    Establishments.forEach((establishments) => {
+        var categoriesNamesObject = {}
+        const filteredProducts = getProducts(establishments.productsId)
         const categoriesIdList = filteredProducts.map(product => product.categoriesId)
         const filteredCategories = getCategories(categoriesIdList)
-        
-        filteredCategories.forEach(category=>{
-            var productsNamesObjects = filteredProducts.filter(product=> product.categoriesId.some(id => id === category.id))
+        filteredCategories.forEach(category=>{           
+            var productsNamesObjects = filteredProducts.filter(product=> {
+                return product.categoriesId.some(id => id === category.id)
+            })
+            var Product = {}
             productsNamesObjects.forEach(product=> {
-                var Product = {}
                 var price = product.price/100
                 Product[product.name] = {price}
-                categoriesNamesObjects[category.name] = Product
+                categoriesNamesObject[category.name] = Product     
             })
         })
-
-        resultJson[establishments.name] = categoriesNamesObjects    
+        resultJson[establishments.name] = categoriesNamesObject   
     });
     return JSON.stringify(resultJson)
 }
-
 function getProducts(arrayIds){
     const products = Products
         .filter(product => arrayIds.some(id => id === product.id))
     return products
 }
-
 function getCategories(arrayIds){
-    //tranformar em funcao
-    var newCategoriesList = []
-    arrayIds.forEach((key,index)=> {
-        if(key.length>1){
-            key.forEach(item=> newCategoriesList.push(item))
-        }else{
-            newCategoriesList.push(key[0])
-        }
-    })
-    newCategoriesList = Array.from(new Set(newCategoriesList))
-    //end função
     const categories = Categories
-         .filter(category => newCategoriesList.some(id => id === category.id))
+         .filter(category => clearArray(arrayIds).some(id => id === category.id))
     return categories
 }
-
+function mean(array){
+    var mean = (array.reduce((a,b)=> a+b)/array.length).toFixed(2)
+    return mean
+}
+function clearArray(array){
+    var newArray = []
+    array.forEach(key=> {
+        if(key.length>1){
+            key.forEach(item=> newArray.push(item))
+        }else{
+            newArray.push(key[0])
+        }
+    })
+    newArray = Array.from(new Set(newArray))
+    return newArray
+}
+console.log(handleJson(data))
